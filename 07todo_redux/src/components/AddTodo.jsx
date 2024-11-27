9f24811fd072bc016c7462e01f2e2e354e758313
sunny-yadav <sunny9405683@gmail.com>
2024-11-28 00:19:15 +0530

    updated the ui

--- 07todo_redux/src/components/AddTodo.jsx
+++ 07todo_redux/src/components/AddTodo.jsx
@@ -1,62 +1,64 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addToTodo,updateMsg } from '../features/todo/todoSlice';
+import { addToTodo, updateMsg } from '../features/todo/todoSlice';
 
-function AddTodo({Reference}) {
+function AddTodo({ Reference }) {
   const [input, setInput] = useState('');
-  const dispatch = useDispatch()
-  const todos = useSelector((state) => state.todos)
-  const update = useSelector((state)=> state.btnUpdate)
-
-
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+  const update = useSelector((state) => state.btnUpdate);
 
   const addTodoHandler = (e) => {
     e.preventDefault();
     if (input === '') {
-      alert("The Msg box is Empty")
+      alert("The Msg box is Empty");
       return;
     }
     for (let obj of todos) {
       if (obj.Msg === input) {
-        alert("You have Enterd The repeated msg")
-        return
+        alert("You have Entered The repeated msg");
+        return;
       }
     }
     dispatch(addToTodo({ Msg: input }));
-    setInput('')
-  }
+    setInput('');
+  };
 
-  const updateTodo = (e)=>{
+  const updateTodo = (e) => {
     e.preventDefault();
     dispatch(updateMsg(input));
-    setInput('')
-    
-  }
+    setInput('');
+  };
 
   return (
-    <form onSubmit={addTodoHandler} className="space-x-3 mt-8 ">
-      <input
-        ref={Reference}
-        type="text"
-        className="w-[82%] text-xl bg-gray-400 placeholder:text-black rounded-xl border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        placeholder="Enter a Todo..."
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      {update.status ?
-       <button
-        onClick={updateTodo}
-        className="text-white  bg-indigo-500 border-0 py-2 px-2 w-fit focus:outline-none hover:bg-indigo-600 rounded-xl text-lg"
-      >
-        Update Todo
-      </button> : <button
-        type="submit"
-        className="text-white  bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded-xl text-lg"
-      >
-        Add Todo
-      </button>}
-    </form>
-  )
+    <div className="max-w-xl mx-auto mt-8 p-6 bg-white rounded-lg shadow-xl">
+      <form onSubmit={addTodoHandler} className="space-y-6">
+        <input
+          ref={Reference}
+          type="text"
+          className="w-full p-4 text-xl bg-gray-200 rounded-xl border-2 border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-600 outline-none transition duration-300 ease-in-out"
+          placeholder="Enter a Todo..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        {update.status ? (
+          <button
+            onClick={updateTodo}
+            className="w-full py-3 px-4 text-lg font-medium text-white bg-indigo-600 rounded-xl transition-colors duration-300 hover:bg-indigo-700 focus:outline-none"
+          >
+            Update Todo
+          </button>
+        ) : (
+          <button
+            type="submit"
+            className="w-full py-3 px-4 text-lg font-medium text-white bg-indigo-600 rounded-xl transition-colors duration-300 hover:bg-indigo-700 focus:outline-none"
+          >
+            Add Todo
+          </button>
+        )}
+      </form>
+    </div>
+  );
 }
 
-export default AddTodo
+export default AddTodo;
